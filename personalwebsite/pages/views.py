b69a1d1ea11bb4ed5dd5780cff2f5b3e25e4aaeb
ahmedghonaim <ahmedghonaim>
2025-05-27 03:40:30 +0300

    mvt home page with about section styles with bootsrap5

---
+++ personalwebsite/pages/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home_page_view(request):
+    return render (request, 'pages/home.html')
+
+def about_page_view(request): 
+    return render(request, "pages/about.html")
